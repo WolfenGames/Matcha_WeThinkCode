cdc390a9627d16f26b10a61c07ec7607cde0f90e
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-11-26 11:39:44 +0200

    auto commit

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const pageRoutes = require('./routes/pages');
 
+app.use(express.static(__dirname + '/../public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
