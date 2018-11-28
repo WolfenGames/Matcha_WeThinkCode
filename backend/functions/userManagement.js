55dd23bdc90fd57a88af0a863bbc327b7c2db909
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-11-28 17:30:56 +0200

    auto commit

--- backend/functions/userManagement.js
+++ backend/functions/userManagement.js
@@ -1,30 +1,33 @@
 const db = require('../database/db');
 
+function deleteByUsername(user) {
+	db.mongo.connect(db.url, { useNewUrlParser: true }).then(db => {
+		var dbo = db.db('Matcha');
+		var query = { username: user};
+		dbo.collection('Users').deleteOne(query).then(res => {
+			db.close();
+		}).catch(err => {
+			console.log("Can't deleteby one (user:: " + user + ") -> " + err);
+		});
+	}).catch(err => {
+		console.log("Cant connect to database called by deleteByUsername(" + user + ")");
+	});
+} 
+function deleteAll() {
+	db.mongo.connect(db.url, { useNewUrlParser: true }).then(db => {
+		var dbo = db.db('Matcha');
+		var query = {type: 'User'};
+		dbo.collection('Users').deleteMany(query).then(res => {
+			db.close();
+		}).catch(err => {
+			console.log("Cant delete many called by deleteAll()");
+		});
+	}).catch(err => {
+		console.log("Cant connect to database called by deleteAll");
+	});
+}
+
 module.exports = {
-	deleteByUsername(user) {
-		/*
-		db.mongo.connect(db.url, {useNewUrlParser: true}, function(err, db) {
-			if (err) throw err;
-			var dbo = db.db('Matcha');
-			var query = { username: user };
-			dbo.collection('Users').deleteOne(query, function(err, res) {
-				if (err) throw err;
-				db.close();
-			});
-		});
-		*/
-	},
-	deleteAll() {
-		/*
-		db.mongo.connect(db.url, {useNewUrlParser: true}, function(err, db) {
-			if (err) throw err;
-			var dbo = db.db('Matcha');
-			var query = { type: 'User' };
-			dbo.collection('Users').deleteMany(query, function(err, res) {
-				if (err) throw err;
-				db.close();
-			});
-		});
-		*/
-	}
+	deleteByUsername: deleteByUsername,
+	deleteAll: deleteAll
 }
--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -5,6 +5,8 @@ const DelteUsers = require('../functions/userManagement');
 const FuncUser = require('../functions/userSave');
 const bcrypt = require('bcrypt');
 
+var message = {};
+
 router.get('/', function(req, res) {
 	res.render('pages/index');
 });
@@ -14,33 +16,41 @@ router.get('/about', function(req, res) {
 });
 
 router.get('/profile', function(req, res) {
-	ListUsers.ListUser((result) => {
-		res.render('pages/profile/profile', { users: result });
-	});
-})
+		res.render('pages/profile/profile');
+});
 
 router.get('/login', function(req, res) {
 	res.render('pages/profile/login');
-})
+});
 
 router.get('/delete/:name', function(req, res) {
-	DelteUsers.deleteByUsername(req.params.name);
-	res.redirect('/profile');
+	message = {};
+	if (!DelteUsers.deleteByUsername(req.params.name))
+	{
+		message = { error: "Failed to delete by username"}
+	}
+	res.redirect('/admin');
 });
 
 router.get('/deleteall', function(req, res) {
 	DelteUsers.deleteAll();
-	res.redirect('/profile');
+	res.redirect('/admin');
 });
 
 router.get('/create', function(req, res) {
 	FuncUser.userSave('Test@gmail.com', 'password', 'User');
-	res.redirect('/profile');
+	res.redirect('/admin');
 });
 
 router.get('/createAdmin', function(req, res) {
-	FuncUser.userSave('Admin@gmail.com', 'password', 'Admin');
-	res.redirect('/profile');
+	message = {};
+	if (!FuncUser.userSave('Admin@gmail.com', 'password', 'Admin'))
+	{
+		message = {
+			error: "Can't create User -> Possible duplicate entry"
+		}
+	}
+	res.redirect('/admin');
 });
 
 router.post('/User/Create', function(req, res) {
@@ -58,8 +68,14 @@ router.get('/signup', function(req, res) {
 	res.render('pages/profile/signup');
 })
 
+router.get('/admin', function(req, res) {
+	ListUsers.ListUser((result) => {
+		res.render('pages/profile/admin', { users: result, message });
+	});
+});
+
 router.get('*', function(req, res) {
 	res.render('pages/404');
-})
+});
 
 module.exports = router;
