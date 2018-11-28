837328cde9df5dd56d0bcb35cb67e129e673c7e7
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-11-28 17:35:33 +0200

    auto commit

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,21 +2,21 @@ const mongo = require('mongodb').MongoClient;
 "use strict";
 
 const url = 'mongodb://localhost:27017/Matcha';
-
+function createCollection(collectionName) {
+	mongo.connect(url, { useNewUrlParser: true }).then(db => {
+		var dbo = db.db('Matcha');
+		dbo.createCollection(collectionName).then(res => {
+			res.createIndex({email: 1}, {unique: true});
+			db.close();
+		}).catch(err => {
+			console.log("Cant create collection {" + collectionName + "} due to -> " + err);
+		});
+	}).catch(err => {
+		console.log("Cant connect to database called by createCollection(" + collectionName + ") due to -> " + err);
+	})
+}
 module.exports = {
 	url,
 	mongo,
-	createCollection(collectionName) {
-		mongo.connect(url, { useNewUrlParser: true }, function(err, db) {
-			console.log("Conneting");
-			if (err) throw err;
-			var dbo = db.db('Matcha');
-			dbo.createCollection(collectionName, function(err, res) {
-				console.log("creating collection " + collectionName);
-				if (err) throw err;
-				res.createIndex({ email: 1}, {unique: true});
-				db.close();
-			});
-		});	
-	}
+	createCollection: createCollection
  };
