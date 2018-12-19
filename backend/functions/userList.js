75fe82102442142699d846b189d8f178858eeb59
Julian Wolf <julian.w16@gmail.com>
2018-12-19 16:53:19 +0200

    Fixed and broke...

--- backend/functions/userList.js
+++ backend/functions/userList.js
@@ -10,44 +10,10 @@ module.exports = {
 						type: "Point",
 						coordinates: user.location
 						},
-					$maxDistance: 10000
+					$maxDistance: 100000
 					}
 				}
 			}
-			var sex, sexuality, usex = user.sex, usexuality = user.sexuality;
-			console.log(usex);
-			console.log(usexuality);
-			//Deciding
-			if (usexuality == '0') {
-				console.log("Sexuality 1");
-				if (usex == '0')
-					sex = '0'
-				if (usex == '1')
-					sex = '1'
-				if (usex == '2')
-					sex = '2';
-			}
-			if (usexuality == '1') {
-				console.log("Sexuality 2");
-				if (usex == '0')
-					sex = '1'
-				if (usex == '1')
-					sex = '0'
-				if (usex == '2')
-					sex = '2'
-			}
-			if (usexuality == '2') {
-				console.log("Sexuality 3");
-				sex = {$or: [{sex: '0', sex: '1', sex: '2'}]};
-			}
-			console.log(sex);
-			console.log(sexuality);
-			query = {
-				location: query.location,
-				sex: sex,
-				sexuality: usexuality
-			}
-			console.log(query);
 			dbo.collection('Users').find(query).sort({age: 1}).toArray().then(result => {
 				fn(result);
 				db.close();
