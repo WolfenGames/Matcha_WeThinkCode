6590b4384b31bf8368890a1c88a93264f6369c56
Julian Wolf <julian.w16@gmail.com>
2019-12-09 13:14:02 +0200

    esFix:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,10 @@
-module.exports = { extends: "plugin:prettier/recommended" };
+module.exports = {
+	extends: "plugin:prettier/recommended",
+	parserOptions: {
+		ecmaVersion: 2017
+	},
+
+	env: {
+		es6: true
+	}
+};
--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,50 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
-const pageRoutes = require('./routes/pages');
-const chatRoutes = require('./routes/chats')
-const session = require('express-session');
-const DB = require('./database/db');
-const fs = require('fs');
-const {CreateChatCollection, CreateRoomCollection} = require('./functions/chat')
-var dir = './public/images';
+const pageRoutes = require("./routes/pages");
+const chatRoutes = require("./routes/chats");
+const session = require("express-session");
+const DB = require("./database/db");
+const fs = require("fs");
+const {
+	CreateChatCollection,
+	CreateRoomCollection
+} = require("./functions/chat");
+var dir = "./public/images";
 
-const { Room } = require('./classes/Room')
-const { Message } = require('./classes/Message')
+const { Room } = require("./classes/Room");
+const { Message } = require("./classes/Message");
 
-
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir);
 }
 
-CreateChatCollection()
-CreateRoomCollection()
+CreateChatCollection();
+CreateRoomCollection();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/../public'));
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/../public"));
+app.set("view engine", "ejs");
 
-app.use(session({secret: "American Pie: Beta House", saveUninitialized: false, resave: false}));
+app.use(
+	session({
+		secret: "American Pie: Beta House",
+		saveUninitialized: false,
+		resave: false
+	})
+);
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader(
-	  "Access-Control-Allow-Headers",
-	  "Origin, X-Requested-With, Content-Type, Accept"
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
 	);
 	res.setHeader(
-	  "Access-Control-Allow-Methods",
-	  "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+		"Access-Control-Allow-Methods",
+		"GET, POST, PATCH, PUT, DELETE, OPTIONS"
 	);
 	next();
 });
@@ -44,15 +52,15 @@ app.use((req, res, next) => {
 app.use("/", pageRoutes);
 app.use("/chat", chatRoutes);
 
-app.post('*', function(req, res) {
+app.post("*", function(req, res) {
 	res.end('{"msg":"404"}');
 });
 
-app.get('*', function(req, res) {
-	res.render('pages/404');
+app.get("*", function(req, res) {
+	res.render("pages/404");
 });
 
-DB.createCollection('Users');
+DB.createCollection("Users");
 DB.createTagsCollection();
 
 module.exports = app;
